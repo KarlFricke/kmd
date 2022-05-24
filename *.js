7149c54679ab65c4e82c2da9b10c630972048cdc
Karl Fricke <101859363+KarlFricke@users.noreply.github.com>
2022-05-24 21:48:51 +0200

    Update *.js

--- *.js
+++ *.js
@@ -1,1 +1,1 @@
-var begriffe = new Array("A2.1 http://192.168.5.240/logbuch_webmodules/stmon?config_id=STATION_A2-1","Birnbaum seiteb.htm#birne","Zausel seitez.htm#zaus");
+var begriffe = new Array("A2.1 seiteb.htm#a2-1","Birnbaum seiteb.htm#birne","Zausel seitez.htm#zaus");
